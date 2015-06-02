fc464bdd39421a6c593072fc5e18e9f8ed66c568
Pedro Simeão Carvalho <pesimeao@gmail.com>
2015-06-02 14:48:37 +0100

    <feat> Add support for image compression

--- gulpfile.js
+++ gulpfile.js
@@ -3,51 +3,74 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
     sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin'),
     foreach = require('gulp-foreach'),
     path = require('path');
 
-// Settings
+// Settings ---------------
+
 var SETTINGS = {
-  dist: {
-    path: {
-      css: './dist/css/',
-      scss: './dist/scss'
-    }
-  },
   scss: {
     src: {
       base: ['./src/scss/**'],
       path: ['./src/scss/**/index.scss']
     },
+    options: {
+      mode: 'default'
+    },
+    dist: {
+      css: {
+        path: './dist/css/' 
+      },
+      scss: {
+        path: './dist/scss' 
+      }
+    },
     watch: {
       path: ['./src/scss/**/*.scss']
+    }
+  },
+  img: {
+    src: {
+      path: ['./src/img/**']
     },
-    compress: false
+    dist: {
+      path: './dist/img'
+    },
+    watch: {
+      path: ['./src/img/**']
+    }
   }
 };
 
-// Tasks
+
+// Tasks ---------------
 
 gulp.task('default', ['build', 'watch']);
-gulp.task('build', ['scss']);
+gulp.task('build', ['scss', 'img']);
 
 gulp.task('watch', function() {
-  
   gulp.watch(SETTINGS.scss.watch.path, ['scss']);
-  
+  gulp.watch(SETTINGS.img.watch.path, ['img']);
 });
 
-// Cleaning
+
+// Cleaning ---------------
 
 gulp.task('clean:css', function() {
-  return gulp.src(SETTINGS.dist.path.scss, {read: false}).pipe(clean({force: true})); 
+  return gulp.src(SETTINGS.scss.dist.css.path, {read: false}).pipe(clean({force: true})); 
 });
 
 gulp.task('clean:scss', function() {
-  return gulp.src(SETTINGS.dist.path.scss, {read: false}).pipe(clean({force: true}));
+  return gulp.src(SETTINGS.scss.dist.scss.path, {read: false}).pipe(clean({force: true}));
 });
 
-// Sass
+gulp.task('clean:img', function() {
+  return gulp.src(SETTINGS.img.dist.path, {read: false}).pipe(clean({force: true}));
+});
+
+
+// Sass ---------------
 
 gulp.task('scss', ['scss:scss', 'scss:css']);
 
@@ -55,7 +78,7 @@ gulp.task('scss:scss', ['clean:scss'], function() {
   
   return gulp
     .src(SETTINGS.scss.src.base)
-    .pipe(gulp.dest(SETTINGS.dist.path.scss));
+    .pipe(gulp.dest(SETTINGS.scss.dist.scss.path));
   
 });
 
@@ -79,6 +102,21 @@ gulp.task('scss:css', ['clean:css'], function() {
     // Sass files
     .pipe(sass.sync().on('error', sass.logError))
     // Output to dist folder
-    .pipe(gulp.dest(SETTINGS.dist.path.css));
+    .pipe(gulp.dest(SETTINGS.scss.dist.css.path));
     
+});
+
+
+// Images ---------------
+
+gulp.task('img', ['clean:img'], function() {
+  
+  return gulp
+    .src(SETTINGS.img.src.path)
+    .pipe(imagemin({
+      // No options
+      // See https://www.npmjs.com/package/gulp-imagemin
+    }))
+    .pipe(gulp.dest(SETTINGS.img.dist.path));
+  
 });
